be755c5aed7383760f4b28470d25f2c50f61446e
mzhong <email@mzhong.cn>
2019-09-06 18:09:54 +0800

    做了一点测试
    concurrent、lamdba

---
+++ test/src/main/java/cn/mzhong/test/concurrent/ConcurrentTest.java
@@ -0,0 +1,60 @@
+package cn.mzhong.test.concurrent;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class ConcurrentTest {
+
+    @Test
+    public void atomicInteger() {
+        AtomicInteger atomicInteger = new AtomicInteger(1);
+        for (int i = 0; i < 1000; i++) {
+            new Thread(atomicInteger::getAndIncrement).start();
+        }
+        System.out.println(atomicInteger.get());
+    }
+
+    @Test
+    public void lock() {
+        class RunService {
+            Lock lock = new ReentrantLock();
+
+            public void run() {
+                lock.lock();
+                System.out.print(Thread.currentThread().getId() + ":");
+                for (int i = 0; i < 10; i++) {
+                    System.out.print("lock:" + i + ",");
+                }
+                System.out.println();
+                lock.unlock();
+            }
+        }
+        long start = System.nanoTime();
+        class RunService2 {
+            public void run() {
+                synchronized (this) {
+                    System.out.print(Thread.currentThread().getId() + ":");
+                    for (int i = 0; i < 10; i++) {
+                        System.out.print("sync:" + i + ",");
+                    }
+                    System.out.println();
+                }
+            }
+        }
+        System.out.println("花费：" + (System.nanoTime() - start));
+        start = System.nanoTime();
+        RunService runService = new RunService();
+        for (int i = 0; i < 10; i++) {
+            new Thread(runService::run).start();
+        }
+        RunService2 runService2 = new RunService2();
+        for (int i = 0; i < 10; i++) {
+            new Thread(runService2::run).start();
+        }
+        System.out.println("花费：" + (System.nanoTime() - start));
+        System.out.println();
+    }
+}
--- test/src/main/java/cn/mzhong/test/lambda/CollectionTest.java
+++ test/src/main/java/cn/mzhong/test/lambda/CollectionTest.java
@@ -1,6 +1,20 @@
 package cn.mzhong.test.lambda;
 
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class CollectionTest {
 
+    @Test
+    public void testFilter() {
+        List<Integer> list = new ArrayList<>();
+        Collections.addAll(list, 1, 2, 3, 4, 5);
+        List<Integer> collect = list.stream().filter(item -> item > 2).collect(Collectors.toList());
+        System.out.println(collect);
+    }
 
 }
